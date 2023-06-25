97d4156c9c767fcfe7866a3f8c39e39fd1798795
Gabor Flamich <gabor.flamich@trendency.hu>
2023-06-25 14:50:23 +0200

    Custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: '{{ text | summary }}',
 })
 export class AppComponent {
-  title = 'pipes';
+  text =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue erat velit, at dignissim libero laoreet at. Fusce sollicitudin suscipit vehicula. Aenean ipsum odio, aliquam et orci vitae, lobortis malesuada leo. Curabitur vestibulum tincidunt tincidunt. Vivamus porta ullamcorper tincidunt. Aenean fermentum elementum erat ultrices scelerisque. Nam vel laoreet diam, et egestas est. Mauris rhoncus est quis arcu scelerisque, quis dictum enim dignissim. Nulla ut nunc risus. Pellentesque blandit erat elit, at ornare arcu congue ac';
 }
---
+++ src/app/shared/pipes/summary.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'summary',
+})
+export class SummaryPipe implements PipeTransform {
+  transform(value: string): string {
+    if (!value) {
+      return value; // If the input is empty, return it as is
+    }
+
+    if (value.length <= 60) {
+      return value; // If the input is already 30 characters or less, return it as is
+    }
+
+    return value.substring(0, 60) + '...'; // Return the substring of the input with ellipsis
+  }
+}
